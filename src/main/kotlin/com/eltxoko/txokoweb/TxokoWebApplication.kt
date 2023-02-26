f24917f96934ceea6c2aadfbb023fdb4573e879f
Jungmin <sjm150@snu.ac.kr>
2023-02-27 00:43:17 +0900

    initial commit

---
+++ src/main/kotlin/com/eltxoko/txokoweb/TxokoWebApplication.kt
@@ -0,0 +1,11 @@
+package com.eltxoko.txokoweb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TxokoWebApplication
+
+fun main(args: Array<String>) {
+	runApplication<TxokoWebApplication>(*args)
+}
---
+++ src/test/kotlin/com/eltxoko/txokoweb/TxokoWebApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.eltxoko.txokoweb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TxokoWebApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
