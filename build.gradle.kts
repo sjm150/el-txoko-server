0be90143fbaa21097cc6196e659d6a51f7295d74
Jungmin <sjm150@snu.ac.kr>
2023-02-28 19:16:38 +0900

    add required dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 	kotlin("jvm") version "1.7.22"
 	kotlin("plugin.spring") version "1.7.22"
 	kotlin("plugin.jpa") version "1.7.22"
+	kotlin("kapt") version "1.7.22"
 }
 
 group = "com.eltxoko"
@@ -19,11 +20,22 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	runtimeOnly("com.mysql:mysql-connector-j")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+	// smtp
+	implementation("org.springframework.boot:spring-boot-starter-mail")
+
+	// querydsl
+	val querydslVersion = "5.0.0"
+	implementation("com.querydsl:querydsl-jpa:$querydslVersion:jakarta")
+	implementation("com.querydsl:querydsl-core:$querydslVersion")
+	kapt("com.querydsl:querydsl-apt:$querydslVersion:jakarta")
+	kapt(group = "com.querydsl", name = "querydsl-apt", classifier = "jpa")
 }
 
 tasks.withType<KotlinCompile> {
@@ -35,4 +47,8 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+tasks.getByName<Jar>("jar") {
+	enabled = false
 }
