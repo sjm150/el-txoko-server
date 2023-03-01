c3da32eedaf039e4b30801385cdf70306d5c3775
Jungmin <sjm150@snu.ac.kr>
2023-03-01 20:53:00 +0900

    add basic spring security setting

---
+++ src/main/kotlin/com/eltxoko/txokoweb/config/AuthConfig.kt
@@ -0,0 +1,22 @@
+package com.eltxoko.txokoweb.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+class AuthConfig {
+
+    @Bean
+    fun securityFilterChain(httpSecurity: HttpSecurity): SecurityFilterChain =
+        httpSecurity
+            .csrf().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .authorizeHttpRequests()
+            .anyRequest().permitAll()
+            .and()
+            .build()
+}
