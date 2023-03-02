950f6cbbac441e1172b78f6d131b7ec5e33f509b
Jungmin <sjm150@snu.ac.kr>
2023-03-02 17:20:11 +0900

    enable jpa auditing

--- src/main/kotlin/com/eltxoko/txokoweb/TxokoWebApplication.kt
+++ src/main/kotlin/com/eltxoko/txokoweb/TxokoWebApplication.kt
@@ -2,8 +2,10 @@ package com.eltxoko.txokoweb
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class TxokoWebApplication
 
 fun main(args: Array<String>) {
