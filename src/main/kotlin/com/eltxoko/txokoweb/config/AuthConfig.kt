7d9ca0894bf96beb62cbd0567098356d74f111f6
Jungmin <sjm150@snu.ac.kr>
2023-03-05 19:47:39 +0900

    add password encoder

--- src/main/kotlin/com/eltxoko/txokoweb/config/AuthConfig.kt
+++ src/main/kotlin/com/eltxoko/txokoweb/config/AuthConfig.kt
@@ -4,10 +4,15 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 class AuthConfig {
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
 
     @Bean
     fun securityFilterChain(httpSecurity: HttpSecurity): SecurityFilterChain =
